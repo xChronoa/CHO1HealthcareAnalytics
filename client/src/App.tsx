17bb9d2e6fcd464bf1ebe6b86b6125d6bf27e97c
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-07-11 13:07:26 +0800

    Initialize React + TypeScript with Vite

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+      <main className="min-h-screen min-w-full flex justify-center items-center bg-black">
+        <h1 className="text-white font-bold">Hello, World!</h1>
+      </main>
+    );
+}
+
+export default App;
