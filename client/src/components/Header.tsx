a34fdceb10c8bd7a6aa17eab8b56cb04f08f1e44
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-07-13 21:19:41 +0800

    feat: added props for managing logo image

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,11 @@
-export default function Footer() {
+type Prop = {
+    logoPath?: string | null;
+}
+
+export default function Header( { logoPath }: Prop) {
     return (
-        <header className="bg-green py-7 shadow-lg"></header>
+        <header className="box-border flex justify-center py-1 shadow-lg bg-green">
+            {logoPath ? <img className="size-12" src={logoPath} alt="City of Cabuyao Logo"/> : ""}
+        </header>
     );
 }
