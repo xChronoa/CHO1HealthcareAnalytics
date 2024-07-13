fd3246c021b7ce1c961577a3a444570450e04cd5
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-07-13 21:26:45 +0800

    feat(style): added font awesome for icon, removed icon images, and update styles
    - Added font awesome for show password icon to enhance loading times.
    - Updated formatting of classes and document formatting.
    - Deleted previously used icon images.

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,37 +4,42 @@ import hide_password from "../assets/hide_password.png";
 import show_password from "../assets/show_password.png";
 import { useState } from "react";
 
+import cabuyao_logo from "../assets/cabuyao_logo.png";
+import { Sidebar } from "../components/Sidebar";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+
 // For specifying the logo to be displayed based on inputted department.
 type LoginProp = {
-    image: string,
-}
+    image: string;
+};
 
-export default function Login( { image } :  LoginProp ) {
+export default function Login({ image }: LoginProp) {
     // Stores password visibility state
     const [showPassword, setShowPassword] = useState(false);
-    const [passwordState, setPasswordState] = useState(hide_password);
-    
+
     // Handles toggling of password visibility
     const togglePassword = () => {
         setShowPassword(!showPassword);
+    };
 
-        if(passwordState.includes(hide_password)) {
-            setPasswordState(show_password);
-        } else {
-            setPasswordState(hide_password);
-        }
-    }
-
-    return(
+    return (
         <>
-            <Header />
+            <Sidebar />
+            <Header logoPath={cabuyao_logo} />
 
             <main className="flex items-center justify-center min-h-screen bg-slate-200">
-
                 <div className="w-11/12 py-16 bg-white shadow-2xl login-container h-3/4 md:w-1/2 min-w-80">
                     <div className="flex flex-col items-center justify-center gap-5 mb-8 title">
-                        <img className="transition-all min-w-max size-50 md:size-60 size-44" src={image} alt="City of Cabuyao Logo" />
-                        <h2 className="text-2xl font-bold text-black uppercase">login</h2>
+                        <img
+                            className="transition-all min-w-max size-50 md:size-60 size-44"
+                            src={image}
+                            alt="City of Cabuyao Logo"
+                        />
+                        <h2 className="text-2xl font-bold text-black uppercase">
+                            login
+                        </h2>
                     </div>
 
                     <form className="flex flex-col items-center justify-center">
@@ -42,7 +47,14 @@ export default function Login( { image } :  LoginProp ) {
                             {/* Username */}
                             <div className="flex flex-col input-group">
                                 <label htmlFor="username">Username</label>
-                                <input className="px-4 py-2 bg-gray-100 shadow-xl" type="text" name="username" id="username" placeholder="Username or Email" required/>
+                                <input
+                                    className="px-4 py-2 bg-gray-100 shadow-xl"
+                                    type="text"
+                                    name="username"
+                                    id="username"
+                                    placeholder="Username or Email"
+                                    required
+                                />
                             </div>
 
                             {/* Password */}
@@ -50,12 +62,39 @@ export default function Login( { image } :  LoginProp ) {
                                 <label htmlFor="password">Password</label>
 
                                 <div className="relative w-full input">
-                                    <input className="w-full px-4 py-2 pr-12 bg-gray-100 shadow-xl" type={showPassword ? "text" : "password"} name="password" id="password" placeholder="Must be 8 characters long" required/>
-                                    <img onClick={togglePassword} src={passwordState} id="toggle-password" onContextMenu={(e) => e.preventDefault()} className="absolute transition-all right-2 top-1 size-8 hover:cursor-pointer hover:scale-95"/>
+                                    <input
+                                        className="w-full px-4 py-2 pr-12 bg-gray-100 shadow-xl"
+                                        type={
+                                            showPassword ? "text" : "password"
+                                        }
+                                        name="password"
+                                        id="password"
+                                        placeholder="Must be 8 characters long"
+                                        required
+                                    />
+                                    {/* <img onClick={togglePassword} src={passwordState} id="toggle-password" onContextMenu={(e) => e.preventDefault()} className="absolute transition-all right-2 top-1 size-8 hover:cursor-pointer hover:scale-95"/> */}
+                                    {showPassword ? (
+                                        <FontAwesomeIcon
+                                            onClick={togglePassword}
+                                            icon={faEye}
+                                            className="absolute transition-all right-2 top-1 size-8 hover:cursor-pointer hover:scale-95"
+                                        />
+                                    ) : (
+                                        <FontAwesomeIcon
+                                            onClick={togglePassword}
+                                            icon={faEyeSlash}
+                                            className="absolute transition-all right-2 top-1 size-8 hover:cursor-pointer hover:scale-95"
+                                        />
+                                    )}
                                 </div>
                             </div>
 
-                            <button type="submit" className="w-full py-2 mt-5 font-bold text-white uppercase rounded-lg shadow-lg bg-green">login</button>
+                            <button
+                                type="submit"
+                                className="w-full py-2 mt-5 font-bold text-white uppercase rounded-lg shadow-lg bg-green"
+                            >
+                                login
+                            </button>
                         </div>
                     </form>
                 </div>
