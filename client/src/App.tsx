47bdb298256294d94771baab2b4e7b1ddb2ddafc
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-07-13 21:28:15 +0800

    feat: implemented routing and draft of private routing
    - Added routing for website pages.
    - Implemented a draft for private routing for authenticated users.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,33 @@
 import Appointment from "./pages/Appointment";
+
 import cabuyao_logo from "./assets/cabuyao_logo.png";
+import cho_logo from "./assets/cho_logo.png";
+
 import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PrivateRoute } from "./utils/PrivateRoute";
+
+// import BarangayDashboard from "./pages/Barangay/BarangayDashboard";
 
 function App() {
     return (
-      <>
-        {/* <Appointment /> */}
-        <Login image={cabuyao_logo}/>
-      </>
+      <div className="relative wrapper">
+        <BrowserRouter>
+          <Routes>
+              <Route element={<Appointment/>} path="/appointment"/>
+
+              <Route element={<Login image={cabuyao_logo}/>} path="/barangay/login"/>
+              <Route element={<Login image={cho_logo}/>} path="/admin/login"/>
+
+              <Route element={<PrivateRoute/>} path="/admin">
+              </Route>
+              
+              <Route element={<PrivateRoute/>} path="/barangay">
+                {/* <Route element={<BarangayDashboard/>}/> */}
+              </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     );
 }
 
