7470e0a38e4eaab0d28920d9c82e7db8c9d3f6bb
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-07-13 16:48:06 +0800

    style: format classes with headwind

--- client/src/pages/Appointment.tsx
+++ client/src/pages/Appointment.tsx
@@ -9,44 +9,44 @@ export default function Appointment() {
         <>
             <Header />
             
-            <main className="bg-almond min-h-screen flex justify-center items-center flex-col">
-                <div className="title flex flex-col justify-center items-center my-5 gap-2">
+            <main className="flex flex-col items-center justify-center min-h-screen bg-almond">
+                <div className="flex flex-col items-center justify-center gap-2 my-5 title">
                     <img className="size-44" src={cabuyao_logo} alt="City of Cabuyao Logo" />
-                    <h2 className="text-black font-bold text-2xl uppercase">book appointment</h2>
+                    <h2 className="text-2xl font-bold text-black uppercase">book appointment</h2>
                 </div>
 
                 
-                <div id="dividing-line" className="bg-black h-1 w-11/12 rounded"></div>
+                <div id="dividing-line" className="w-11/12 h-1 bg-black rounded"></div>
                 
                 {/* Appointment Information */}
-                <div className="appointment-details w-4/5 flex flex-col justify-center items-center">
-                    <h2 className="font-bold self-center md:self-start uppercase text-lg my-5">Personal Information</h2>
+                <div className="flex flex-col items-center justify-center w-4/5 appointment-details">
+                    <h2 className="self-center my-5 text-lg font-bold uppercase md:self-start">Personal Information</h2>
                     
-                    <form id="appointment" className="flex flex-col justify-center min-w-96 w-3/4 mt-2 mb-16 p-5 px-8 border-black border-2 rounded">
+                    <form id="appointment" className="flex flex-col justify-center w-3/4 p-5 px-8 mt-2 mb-16 border-2 border-black rounded min-w-96">
                         {/* Full Name */}
-                        <div className="input-group flex flex-row flex-wrap lg:flex-nowrap mb-3 gap-5">
-                            <div className="first-name flex items-center gap-2 w-full">
+                        <div className="flex flex-row flex-wrap gap-5 mb-3 input-group lg:flex-nowrap">
+                            <div className="flex items-center w-full gap-2 first-name">
                                 <label className="min-w-24" htmlFor="first-name">First Name:</label>
                                 <input className="w-full" type="text" name="first-name" id="first-name" placeholder="First Name" required/>
                             </div>
 
-                            <div className="last-name flex items-center gap-2 w-full">
+                            <div className="flex items-center w-full gap-2 last-name">
                                 <label className="min-w-24" htmlFor="last-name">Last Name:</label>
                                 <input className="w-full" type="text" name="last-name" id="last-name" placeholder="Last Name" required/>
                             </div>
                         </div>
 
                         {/* Sex */}
-                        <div className="input-group flex flex-col mb-3 gap-2">
+                        <div className="flex flex-col gap-2 mb-3 input-group">
                             <label htmlFor="sex">Sex:</label>
                             
-                            <div className="sex-group flex justify-evenly flex-row gap-5">
-                                <div className="male flex gap-2">
+                            <div className="flex flex-row gap-5 sex-group justify-evenly">
+                                <div className="flex gap-2 male">
                                     <input type="radio" name="sex" id="male" value="Male" required/>
                                     <label htmlFor="male">Male</label>
                                 </div>
 
-                                <div className="female flex gap-2">
+                                <div className="flex gap-2 female">
                                     <input type="radio" name="sex" id="female" value="Female" required/>
                                     <label htmlFor="female">Female</label>
                                 </div>
@@ -54,25 +54,25 @@ export default function Appointment() {
                         </div>
 
                         {/* Date of Birth */}
-                        <div className="input-group flex flex-col mb-3">
+                        <div className="flex flex-col mb-3 input-group">
                             <label htmlFor="birthdate">Date of Birth:</label>
                             <input className='w-full' type="date" name="birthdate" id="birthdate" required/>
                         </div>
 
                         {/* Address */}
-                        <div className="input-group flex flex-col mb-3">
+                        <div className="flex flex-col mb-3 input-group">
                             <label htmlFor="address">Address:</label>
                             <input type="text" name="address" id="address" placeholder="House No. / Street / Barangay"required/>
                         </div>
 
                         {/* Appointment Date */}
-                        <div className="input-group flex flex-col mb-3">
+                        <div className="flex flex-col mb-3 input-group">
                             <label htmlFor="appointment-date">Appointment Date:</label>
                             <input className='w-full' type="datetime-local" name="appointment-date" id="appointment-date" required/>
                         </div>
 
                         {/* Type of Appointment */}
-                        <div className="input-group flex flex-col mb-3">
+                        <div className="flex flex-col mb-3 input-group">
                             <label htmlFor="appointment-type">Type of Appointment:</label>
                             <select className="p-2" name="appointment-type" id="appointment-type" required>
                                 <option value={1}>1</option>
@@ -89,34 +89,34 @@ export default function Appointment() {
                         </div>
 
                         {/* Email */}
-                        <div className="input-group flex flex-col mb-3">
+                        <div className="flex flex-col mb-3 input-group">
                             <label htmlFor="email">Email:</label>
                             <input className="" type="email" name="email" id="email" placeholder="example@email.com" required/>
                         </div>
 
                         {/* Contact Number */}
-                        <div className="input-group flex flex-col mb-3">
+                        <div className="flex flex-col mb-3 input-group">
                             <label htmlFor="phone-number">Contact Number:</label>
-                            <div className="input w-full relative">
+                            <div className="relative w-full input">
                                 <input className="w-full pr-28" type="text" name="phone-number" id="phone-number" placeholder="+1 (123) 456-7890" required/>
-                                <button className="absolute right-2 top-1 bg-green text-white p-1 px-3 rounded-full shadow-xl hover:opacity-75 transition-all">Send OTP</button>
+                                <button className="absolute p-1 px-3 text-white transition-all rounded-full shadow-xl right-2 top-1 bg-green hover:opacity-75">Send OTP</button>
                             </div>
                         </div>
 
                         {/* OTP */}
-                        <div className="input-group flex flex-col mb-3">
+                        <div className="flex flex-col mb-3 input-group">
                             <label htmlFor="otp">OTP</label>
                             <input type="text" name="otp" id="otp" placeholder="123456" required/>
                         </div>
 
                         {/* Patient Note */}
-                        <div className="input-group flex flex-col mb-3">
+                        <div className="flex flex-col mb-3 input-group">
                             <textarea className="p-2" name="note" id="note" cols={50} rows={10} placeholder="Leave a note"></textarea>
                         </div>
 
                         {/* Terms & Conditions */}
-                        <div className="terms flex flex-row justify-center items-center my-2">
-                            <div className="wrap flex flex-row gap-5 w-full md:w-3/4">
+                        <div className="flex flex-row items-center justify-center my-2 terms">
+                            <div className="flex flex-row w-full gap-5 wrap md:w-3/4">
                                 <input className="scale-150" type="checkbox" name="terms" id="terms" required/>
                                 <label htmlFor="terms">
                                     <p className="font-bold text-justify">
@@ -126,7 +126,7 @@ export default function Appointment() {
                             </div>
                         </div>
 
-                        <button className="uppercase bg-green text-white font-bold p-2 my-5 rounded-lg hover:opacity-75 transition-all shadow-xl" type="submit">Submit</button>
+                        <button className="p-2 my-5 font-bold text-white uppercase transition-all rounded-lg shadow-xl bg-green hover:opacity-75" type="submit">Submit</button>
                     </form>
                 </div>
             </main>
