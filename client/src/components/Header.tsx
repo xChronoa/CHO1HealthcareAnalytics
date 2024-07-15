154a626642248ad9921808ac582089a83b12dfb2
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-07-15 21:28:09 +0800

    feat(style): added menu bar, additional props, and convert syntax to typescript
    - Added optional menu bar and menu bar function prop for future use in guest and authenticated user view.
    - Refactored code syntax to better fit typescript type safety convention.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,29 @@
-type Prop = {
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface HeaderProp {
     logoPath?: string | null;
+    collapseSidebar?: () => void;
 }
 
-export default function Header( { logoPath }: Prop) {
+export const Header: React.FC<HeaderProp> = ({ logoPath, collapseSidebar }) => {
     return (
-        <header className="box-border flex justify-center py-1 shadow-lg bg-green">
-            {logoPath ? <img className="size-12" src={logoPath} alt="City of Cabuyao Logo"/> : ""}
+        <header className={`box-border flex items-center justify-between w-full px-8 ${logoPath ? "py-2" : "py-8"} shadow-lg bg-green`}>
+            <FontAwesomeIcon
+                icon={faBars}
+                className="self-center p-[4px] lg:hidden cursor-pointer hover:bg-lime-800 transition-all duration-300 text-white border-2 border-white rounded-lg size-6"
+                onClick={collapseSidebar}
+            />
+
+            <div className="flex justify-center flex-1">
+                {logoPath && (
+                    <img
+                        className="self-center size-12"
+                        src={logoPath}
+                        alt="City of Cabuyao Logo"
+                    />
+                )}
+            </div>
         </header>
     );
-}
+};
