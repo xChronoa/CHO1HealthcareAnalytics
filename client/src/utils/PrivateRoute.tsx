b464383e4db8abfceac1917cdf4ed04120c6fbe2
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-07-31 01:20:30 +0800

    feat: added additional conditions to redirect to specific login page based on previous url

--- client/src/utils/PrivateRoute.tsx
+++ client/src/utils/PrivateRoute.tsx
@@ -1,11 +1,17 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 
 export const PrivateRoute: React.FC = () => {
-    const user: boolean = false;
+    // Simulating user authentication status; this should be replaced with actual authentication logic
+    const user: boolean = false; // false indicates the user is not authenticated
+    const location = useLocation();
+    
+    // Determine the type of login needed based on the current path
+    const loginPath = location.pathname.startsWith("/admin") ? "/admin/login" : "/barangay/login";
+
 
     return(
         <>
-            {user ? <Outlet/> : <Navigate to={"login"}/>}
+            {user ? <Outlet/> : <Navigate to={loginPath}/>}
         </>
     );
 };
