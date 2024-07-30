0541096737ee494b18c127f78b4427b51705670e
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-07-31 01:18:25 +0800

    refactor(style): changed export structure and changed z-index order

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,11 @@
-export const Footer: React.FC = () => {
+const Footer: React.FC = () => {
     return (
-        <footer className="sticky z-50 py-4 bg-gray-700">
+        <footer className="sticky z-40 py-4 bg-gray-700">
             <h2 className="text-center text-white">
                 &copy;2024 Cabuyao City Health Office 1. All rights reserved.
             </h2>
         </footer>
     );
 };
+
+export default Footer;
