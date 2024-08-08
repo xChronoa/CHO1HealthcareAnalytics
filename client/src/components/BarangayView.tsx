d355a8d8ee95d27d1a339413002a1bf9cefe4729
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-08-08 11:56:01 +0800

    feat: implemented the component for individual barangay for the barangay list page

---
+++ client/src/components/BarangayView.tsx
@@ -0,0 +1,16 @@
+interface BarangayViewProp {
+    logoPath: string;
+}
+
+const BarangayView: React.FC<BarangayViewProp> = ({ logoPath }) => {
+    return (
+        <div className="flex flex-col items-center justify-center gap-2 transition-all">
+            <img src={logoPath} alt="Logo" className="rounded-full shadow-xl md:scale-90 shadow-neutral-400" />
+            <button className="w-full px-5 py-2 uppercase transition-all bg-white rounded-lg shadow-md border-[2px] hover:bg-green hover:border-white hover:text-white shadow-gray-500">
+                View
+            </button>
+        </div>
+    );
+};
+
+export default BarangayView;
