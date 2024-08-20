860db65c30b8ed3eec6279ec75b992028400e622
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-08-20 11:15:38 +0800

    feat: draft seeders for testing authentication

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::create([
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'email' => 'admin@email.com',
+            'role' => 'Admin',
+            'status' => 'active'
+        ]);
+
+        User::create([
+            'username' => 'encoder',
+            'password' => Hash::make('encoder'),
+            'email' => 'encoder@email.com',
+            'role' => 'Encoder',
+            'status' => 'active'
         ]);
     }
 }
