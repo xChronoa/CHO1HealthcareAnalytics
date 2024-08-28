f6551baa6372981100ca21f5c0405efa57fe6c28
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-08-28 21:33:27 +0800

    feat: adjusted alt attribute

--- client/src/components/BarangayView.tsx
+++ client/src/components/BarangayView.tsx
@@ -5,7 +5,7 @@ interface BarangayViewProp {
 const BarangayView: React.FC<BarangayViewProp> = ({ logoPath }) => {
     return (
         <div className="flex flex-col items-center justify-center gap-2 transition-all">
-            <img src={logoPath} alt="Logo" className="rounded-full shadow-xl md:scale-90 shadow-neutral-400" />
+            <img src={logoPath} alt="Barangay Logo" className="rounded-full shadow-xl md:scale-90 shadow-neutral-400" />
             <button className="w-full px-5 py-2 uppercase transition-all bg-white rounded-lg shadow-md border-[2px] hover:bg-green hover:border-white hover:text-white shadow-gray-500">
                 View
             </button>
