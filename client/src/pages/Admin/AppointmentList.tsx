9783d826faa082e1ddf5e5e1cefe842c14498ec7
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-08-28 21:46:02 +0800

    feat: implemented the functionalities for fetching and displaying the patients for an appointment date and appointment type

--- client/src/pages/Admin/AppointmentList.tsx
+++ client/src/pages/Admin/AppointmentList.tsx
@@ -1,74 +1,194 @@
+import { useState } from "react";
+import { useAppointmentCategory } from "../../hooks/useAppointmentCategory";
+import { useAppointment } from "../../hooks/useAppointment"; // Hook for fetching appointments
+import useEffectAfterMount from "../../hooks/useEffectAfterMount";
+import Loading from "../../components/Loading";
+
 const AppointmentList: React.FC = () => {
+    const {
+        fetchAppointmentCategories,
+        appointmentCategories,
+        appointmentCategoryLoading,
+    } = useAppointmentCategory();
+    const {
+        fetchAppointmentsByCategory,
+        fetchPatientsAppointments,
+        appointments,
+        appointmentLoading,
+        error,
+        minDate,
+        maxDate,
+    } = useAppointment(); // Use the appointments hook
+    
+    const [selectedAppointmentType, setSelectedAppointmentType] =
+        useState<string>("");
+    const [selectedDate, setSelectedDate] = useState<string>(maxDate || "");
+    
+    // Fetch appointment categories on component mount
+    useEffectAfterMount(() => {
+        fetchAppointmentCategories();
+    }, [fetchAppointmentCategories]);
+    
+    // Fetch appointments when selectedAppointmentType or selectedDate changes
+    useEffectAfterMount(() => {
+        if (selectedAppointmentType && selectedAppointmentType !== "All") {
+            fetchAppointmentsByCategory(selectedAppointmentType, selectedDate);
+
+        } else {
+            fetchPatientsAppointments();
+            console.log(appointments);
+            
+            // Update the date range if maxDate is available
+            if (maxDate) {
+                setSelectedDate(maxDate);
+            }
+        }
+    }, [selectedAppointmentType, selectedDate, fetchAppointmentsByCategory, fetchPatientsAppointments, maxDate]);
+    
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedAppointmentType(event.target.value);
+    };
+    
+    const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedDate(event.target.value);
+    };
+    
     return (
-        <div className="w-11/12 py-16">
-            <header className="mb-4 ">
-                <h1 className="mb-2 text-2xl font-bold">Patient Appointments</h1>
-                <div className="dividing-line w-full h-[2px] bg-black"></div>
-            </header>
-            <section className="flex flex-col items-start">
-                <div className="flex flex-row items-center justify-end w-full gap-10 mt-6 mb-5 sm:mb-0 filter">
-                    {/* Filter by month and year */}
-                    <div className="relative flex flex-row items-center justify-center gap-2">
-                        <label htmlFor="date" className="absolute left-0 top-[-1.5rem] font-medium">Appointment Date</label>
-                        <input
-                            className="p-1 py-1.5 text-center bg-gray-100 border border-gray-300 rounded-lg shadow-lg border-1"
-                            type="date"
-                            name="date"
-                            id="date"
-                        />
-                    </div>
+        <>
+            <div className="w-11/12 py-16">
+                <header className="mb-4">
+                    <h1 className="mb-2 text-2xl font-bold">
+                        Patient Appointments
+                    </h1>
+                    <div className="dividing-line w-full h-[2px] bg-black"></div>
+                </header>
 
-                    <div className="flex flex-row items-center justify-center gap-5">
-                        <select
-                                className="px-2 py-2 bg-gray-100 border border-gray-300 rounded-lg shadow-lg border-1"
-                                name="barangay"
-                                id="barangay"
-                                required
+                <section className="flex flex-col items-start">
+                    <div className="flex flex-row items-center justify-end w-full gap-10 mt-6 mb-5 sm:mb-0 filter">
+                        {/* Filter by month and year */}
+                        <div className="relative flex flex-row items-center justify-center gap-2">
+                            <label
+                                htmlFor="date"
+                                className="absolute left-0 top-[-1.5rem] font-medium w-full text-nowrap"
                             >
-                            <option hidden>Select Category</option>
-                            <option value="">1</option>
-                            <option value="">2</option>
-                            <option value="">3</option>
-                            <option value="">4</option>
-                            <option value="">5</option>
-                        </select>
-                    </div>
-                </div>
+                                Appointment Date
+                            </label>
+                            <input
+                                className="p-1 py-1.5 text-center bg-gray-100 border border-gray-300 rounded-lg shadow-lg border-1"
+                                type="date"
+                                name="date"
+                                id="date"
+                                min={minDate}
+                                max={maxDate}
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                            />
+                        </div>
 
-                <div className="w-full p-4 overflow-x-auto rounded-lg shadow-md sm:py-4 table-container outline outline-1 shadow-black sm:outline-0 sm:shadow-transparent">
-                    <table className="w-full text-center">
-                        {/* Table Header */}    
-                        <thead className="bg-white rounded-[16px] shadow-lg outline outline-1 outline-black uppercase">
-                            <tr>
-                                <th className="px-4 py-2 rounded-tl-[16px] rounded-bl-[16px]">Name</th>
-                                <th className="px-4 py-2">Sex</th>
-                                <th className="px-4 py-2">Birthdate</th>
-                                <th className="px-4 py-2">Address</th>
-                                <th className="px-4 py-2">Email</th>
-                                <th className="px-4 py-2">Phone#</th>
-                                <th className="px-4 py-2 rounded-tr-[16px] rounded-br-[16px]">
-                                    Time
-                                </th>
-                            </tr>
-                        </thead>
-                        {/* Table Contents */}
-                        <tbody>
-                            {/* Dummy Data */}
-                            <tr>
-                                <td className="px-4 py-2 font-semibold uppercase">John Doe</td>
-                                <td className="px-4 py-2 font-semibold uppercase">Male</td>
-                                <td className="px-4 py-2 font-semibold uppercase">November 1, 2000</td>
-                                <td className="px-4 py-2 font-semibold uppercase">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor pariatur nulla similique quis dolorum sit distinctio autem. Soluta, numquam! Quidem?</td>
-                                <td className="px-4 py-2 font-semibold uppercase">emailemailemailemailemailemail@email.email</td>
-                                <td className="px-4 py-2 font-semibold uppercase">0123 456 7890</td>
-                                <td className="px-4 py-2 font-semibold uppercase">01:00 PM</td>
-                            </tr>
-                            
-                        </tbody>
-                    </table>
-                </div>
-            </section>
-        </div>
+                        <div className="flex flex-row items-center justify-center gap-5">
+                            <select
+                                className="w-full px-2 py-2 bg-gray-100 border border-gray-300 rounded-lg shadow-lg md:w-54 border-1"
+                                name="appointment-type"
+                                id="appointment-type"
+                                required
+                                value={selectedAppointmentType}
+                                onChange={handleChange}
+                            >
+                                <option value="All">All</option>
+                                {appointmentCategories.map((category) => (
+                                    <option
+                                        key={category.appointment_category_id}
+                                        value={
+                                            category.appointment_category_name
+                                        }
+                                    >
+                                        {category.appointment_category_name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="w-full p-4 overflow-x-auto rounded-lg shadow-md sm:py-4 table-container outline outline-1 shadow-black sm:outline-0 sm:shadow-transparent">
+                        <table className="w-full text-center">
+                            {/* Table Header */}
+                            <thead className="bg-white rounded-[16px] shadow-lg outline outline-1 outline-black uppercase">
+                                <tr>
+                                    <th className="px-4 py-2 rounded-tl-[16px] rounded-bl-[16px]">
+                                        Name
+                                    </th>
+                                    <th className="px-4 py-2">Sex</th>
+                                    <th className="px-4 py-2">Birthdate</th>
+                                    <th className="px-4 py-2">Address</th>
+                                    <th className="px-4 py-2">Email</th>
+                                    <th className="px-4 py-2 rounded-tr-[16px] rounded-br-[16px]">
+                                        Phone#
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {appointmentLoading ? (
+                                    <tr>
+                                        <td
+                                            colSpan={6}
+                                            className="px-4 py-2 text-center"
+                                        >
+                                            Loading appointments...
+                                        </td>
+                                    </tr>
+                                ) : error ? (
+                                    <tr>
+                                        <td
+                                            colSpan={6}
+                                            className="px-4 py-2 text-center text-red-500"
+                                        >
+                                            Error loading appointments: {error}
+                                        </td>
+                                    </tr>
+                                ) : appointments.length === 0 ? (
+                                    <tr>
+                                        <td
+                                            colSpan={6}
+                                            className="px-4 py-2 text-center"
+                                        >
+                                            No appointments found for the
+                                            selected category.
+                                        </td>
+                                    </tr>
+                                ) : (
+                                    appointments.map((appointment) => (
+                                        <tr key={appointment.patient.patient_id}>
+                                            <td className="px-4 py-2 font-semibold">
+                                                {appointment.patient.first_name} {appointment.patient.last_name}
+                                            </td>
+                                            <td className="px-4 py-2 font-semibold">
+                                                {appointment.patient.sex}
+                                            </td>
+                                            <td className="px-4 py-2 font-semibold">
+                                                {appointment.patient.birthdate}
+                                            </td>
+                                            <td className="px-4 py-2 font-semibold">
+                                                {appointment.patient.address}
+                                            </td>
+                                            <td className="px-4 py-2 font-semibold">
+                                                {appointment.patient.email}
+                                            </td>
+                                            <td className="px-4 py-2 font-semibold">
+                                                {
+                                                    appointment.patient
+                                                        .phone_number
+                                                }
+                                            </td>
+                                        </tr>
+                                    ))
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
+            {appointmentLoading && appointmentCategoryLoading && <Loading />}
+        </>
     );
 };
 
