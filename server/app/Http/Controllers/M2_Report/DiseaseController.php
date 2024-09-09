67762654bb23389955e03a56c11c51d1693b796f
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-09-10 01:07:38 +0800

    feat: created method for retrieving the diseases

--- server/app/Http/Controllers/M2_Report/DiseaseController.php
+++ server/app/Http/Controllers/M2_Report/DiseaseController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\M2_Report;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\M2_Report\Disease;
 
 class DiseaseController extends Controller
 {
-    //
+    public function index() {
+        $diseases = Disease::select("disease_id", "disease_name", "disease_code")->get();
+
+        return response()->json($diseases);
+    }
 }
