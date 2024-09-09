f9ba3f97af400131dd8847b74d6abb5176227222
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-09-10 01:08:08 +0800

    feat: created methods for retrieving all services or a service by service name

--- server/app/Http/Controllers/M1_Report/ServiceController.php
+++ server/app/Http/Controllers/M1_Report/ServiceController.php
@@ -4,7 +4,27 @@ namespace App\Http\Controllers\M1_Report;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\M1_Report\Service;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class ServiceController extends Controller
 {
-    //
+    public function index() {
+        $services = Service::select("service_id", "service_name")->get();
+
+        return response()->json($services);
+    }
+
+    public function show(Request $request, $service_name) {
+        try {
+            // Fetch the service by name from the database
+            $service = Service::where('service_name', $service_name)->firstOrFail();
+    
+            // Return the service data as JSON
+            return response()->json($service);
+        } catch (ModelNotFoundException $e) {
+            // Return a 404 error if the service is not found
+            return response()->json(['error' => 'Service not found'], 404);
+        }
+    }
 }
