e42aebe93aa5c50da943b8d2518cb08ed3dbe155
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-09-10 01:07:13 +0800

    feat: added method for retrieving the age categories

--- server/app/Http/Controllers/AgeCategoryController.php
+++ server/app/Http/Controllers/AgeCategoryController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AgeCategory;
 use Illuminate\Http\Request;
 
 class AgeCategoryController extends Controller
 {
-    //
+    public function index() {
+        $ageCategories = AgeCategory::select("age_category_id", "age_category")->get();
+
+        return response()->json($ageCategories);
+    }
 }
