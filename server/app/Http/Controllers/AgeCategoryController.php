8259562a888e4baa961f183853dbe42e49b98091
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-09-10 10:34:47 +0800

    feat: limit to categories for morbidity report

--- server/app/Http/Controllers/AgeCategoryController.php
+++ server/app/Http/Controllers/AgeCategoryController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class AgeCategoryController extends Controller
 {
     public function index() {
-        $ageCategories = AgeCategory::select("age_category_id", "age_category")->get();
+        $ageCategories = AgeCategory::select("age_category_id", "age_category")->limit(18)->get();
 
         return response()->json($ageCategories);
     }
