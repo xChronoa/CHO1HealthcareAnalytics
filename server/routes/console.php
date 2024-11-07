23902830ddab212afb52dc7af9da3ff7c13f9f7f
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-11-07 12:52:43 +0800

    feat: added schedule call to check pending reports

--- server/routes/console.php
+++ server/routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    Artisan::call('app:send-pending-report-notice --check');
+})->dailyAt('00:00');
