883398180889a8df1c94e15936a35558e215e0da
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-11-18 11:54:04 +0800

    feat: added password confirmation attribute

--- client/src/types/User.tsx
+++ client/src/types/User.tsx
@@ -2,6 +2,7 @@ export interface User {
     user_id?: number;
     username: string;
     password?: string;
+    password_confirmation?: string;
     email: string;
     barangay_name: string;
     role?: string;
