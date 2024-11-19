ad7ea47ca2607e2b54b2b57227fa22841a171a35
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-11-20 04:34:08 +0800

    style: lowered z index to accomodate recaptcha

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
     return (
-        <footer className="sticky z-40 py-4 bg-gray-700">
+        <footer className="sticky z-30 py-4 bg-gray-700">
             <h2 className="text-center text-white">
                 &copy;2024 Cabuyao City Health Office 1. All rights reserved.
             </h2>
