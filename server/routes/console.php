fae4eada434a8103e3a7e38ff47b14e05309ab3b
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-11-20 04:43:59 +0800

    feat: schedules for cron job

--- server/routes/console.php
+++ server/routes/console.php
@@ -4,10 +4,18 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::call(function () {
     Artisan::call('app:send-pending-report-notice --check');
-})->dailyAt('00:00');
+})->dailyAt('06:00')->name('send-pending-report-notice-06:00')->withoutOverlapping();
+
+Schedule::call(function () {
+    Artisan::call('app:send-pending-report-notice --check');
+})->dailyAt('18:00')->name('send-pending-report-notice-18:00')->withoutOverlapping();
+
+Schedule::call(function () {
+    Artisan::call('queue:work --stop-when-empty');
+})->everyMinute()->name('queue-worker')->withoutOverlapping();
