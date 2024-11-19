ff02fcd7201fa1490cc039070268629165f0cc1c
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-11-20 04:35:11 +0800

    feat: changed button to link

--- client/src/components/SuccessNewPassword.tsx
+++ client/src/components/SuccessNewPassword.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 interface SuccessNewPasswordProps {
     role: string;
@@ -63,13 +63,12 @@ const SuccessNewPassword: React.FC<SuccessNewPasswordProps> = ({
                         </p>
                     </div>
                     <div className="flex justify-between gap-2">
-                        <button
-                            type="button"
-                            onClick={handleLoginRedirect}
+                        <Link
+                            to={role === "admin" ? "/admin/login" : role === "encoder" ? "/barangay/login" : "/"}
                             className="transition-all text-[.7rem] sm:text-sm text-white w-full bg-green hover:bg-[#009900] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                         >
                             Continue to Login
-                        </button>
+                        </Link>
                     </div>
                 </section>
             </div>
